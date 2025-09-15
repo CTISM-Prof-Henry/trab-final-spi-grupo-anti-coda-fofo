73c4cc270841abb44b2f32f271f8a8157e0c674d
Jonas <jonassilvadelara@gmail.com>
2025-09-15 12:34:08 -0300

    reconfiguração para testes de banco, spring swagger e documentação, flyway e validate, orm completo, controller e service sendo implementados, melhorar consultas com query

--- backend/src/main/java/com/ufsm/politecnico/model/agendamento/AgendamentoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ufsm.politecnico.model.agendamento;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface AgendamentoRepository extends JpaRepository<Agendamento, UUID> {
-}
--- backend/src/main/java/com/ufsm/politecnico/model/sala/SalaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ufsm.politecnico.model.sala;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface SalaRepository extends JpaRepository<Sala, UUID> {
-}
