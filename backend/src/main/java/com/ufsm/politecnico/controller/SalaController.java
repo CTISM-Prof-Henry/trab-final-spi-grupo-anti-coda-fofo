facbc78e65b826a40b562b6d06b04cc89519d479
Aluno <aluno@gmail.com>
2025-09-16 15:38:45 -0300

    Controller, service de sala, adição de enum de predios

--- backend/src/main/java/com/ufsm/politecnico/controller/SalaController.java
+++ backend/src/main/java/com/ufsm/politecnico/controller/SalaController.java
@@ -1,5 +1,42 @@
 package com.ufsm.politecnico.controller;
 
+
+import com.ufsm.politecnico.model.Sala;
+import com.ufsm.politecnico.repositories.SalaRepository;
+import com.ufsm.politecnico.service.SalaService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/sala")
 public class SalaController {
 
+    private final SalaService salaService;
+
+    public SalaController(SalaService salaService) {
+        this.salaService = salaService;
+    }
+
+
+    @GetMapping
+    public List<Sala> sala(){
+        return salaService.getSalas();
+    }
+
+    @GetMapping("{id}")
+    public Sala getSala(@PathVariable long id){
+        return salaService.getSala(id);
+    }
+
+    @PostMapping
+    public Sala sala(@Valid @RequestBody Sala sala){
+        return salaService.createSala(sala);
+    }
+
+    @DeleteMapping("{id}")
+    public Boolean salaDelete(@PathVariable long id){
+        return salaService.deleteSala(id);
+    }
 }
