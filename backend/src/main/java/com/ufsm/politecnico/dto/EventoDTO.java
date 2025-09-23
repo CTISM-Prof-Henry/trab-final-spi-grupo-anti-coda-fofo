591e53b823d9d122bb50c830b8ef205874e1f1ce
Jonas <jonassilvadelara@gmail.com>
2025-09-23 17:39:44 -0300

    controller + service + dto de eventos

---
+++ backend/src/main/java/com/ufsm/politecnico/dto/EventoDTO.java
@@ -0,0 +1,16 @@
+package com.ufsm.politecnico.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EventoDTO {
+    private Long id;
+    private String nome;
+    private String tipo;
+}
--- backend/src/main/java/com/ufsm/politecnico/service/EventoService.java
+++ backend/src/main/java/com/ufsm/politecnico/service/EventoService.java
@@ -1,5 +1,36 @@
 package com.ufsm.politecnico.service;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.ufsm.politecnico.dto.EventoDTO;
+import com.ufsm.politecnico.model.Evento;
+import com.ufsm.politecnico.repositories.EventoRepository;
+
+@Service
 public class EventoService {
 
+    private EventoRepository eventoRepository;
+
+    public EventoService(EventoRepository eventoRepository){
+        this.eventoRepository = eventoRepository;
+    }
+
+    public ArrayList<EventoDTO> finDtos(){
+        ArrayList<EventoDTO> eventoDTOs = new ArrayList<EventoDTO>();
+        List<Evento> eventos = this.eventoRepository.findAll();
+        Iterator<Evento> i = eventos.iterator();
+        while (i.hasNext()) {
+            Evento e = i.next();
+            eventoDTOs.add(new EventoDTO(
+                e.getId(),
+                e.getNome(),
+                e.getTipo().name()
+            ));
+        }
+        return eventoDTOs;
+    }
 }
