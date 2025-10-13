a0bc10806f9db8649fd21bccda27554d00e88518
Bruno Muniz <devbrunomuniz@gmail.com>
2025-10-13 10:37:39 -0300

    Inserção das demais homes restastes

--- frontend/src/app/pages/home-alunos/home-alunos.ts
+++ frontend/src/app/pages/home-alunos/home-alunos.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-home-alunos',
-  imports: [],
+    imports: [
+        NgIf
+    ],
   templateUrl: './home-alunos.html',
   styleUrl: './home-alunos.css'
 })
 export class HomeAlunos {
+  activeSection: string = 'home';
+
+  setSection(section: string) {
+    this.activeSection = section;
+  }
 
 }
