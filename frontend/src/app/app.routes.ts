3d46cc10573f3f7cd4e40c74121c6a5209171547
Brunogmuniz <devbrunomuniz@gmail.com>
2025-11-09 14:37:11 -0300

    Inserção do Login

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { HomeComponent } from './pages/home/home';
 import { HomeAdmin } from './pages/home-admin/home-admin';
 import {HomeProfessores} from './pages/home-professores/home-professores';
 import {HomeAlunos} from './pages/home-alunos/home-alunos';
+import {Login} from './pages/login/login';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'admin', component: HomeAdmin },
   { path: 'professores', component: HomeProfessores },
   { path: 'alunos', component: HomeAlunos },
+  { path: 'login', component: Login },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '**', redirectTo: 'home' }
 ];
