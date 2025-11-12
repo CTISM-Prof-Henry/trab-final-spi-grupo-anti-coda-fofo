1f8d921123361c6740dd245426370ec20e25cff4
Iuri <iuriguilhermesantos@gmail.com>
2025-11-12 12:02:52 -0300

    fix: add locahost:4200 em allowedOrigins

--- backend/src/main/java/com/politecnico/poliagenda/config/CorsConfig.java
+++ backend/src/main/java/com/politecnico/poliagenda/config/CorsConfig.java
@@ -6,6 +6,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.List;
+
 /*cria a configuração de um cors para acessar a api por fora e aplica
  a todos os endpoints*/
 @Configuration
@@ -15,6 +17,8 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         //configuration.addAllowedOriginPattern("https://meusite.com");
+        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        //configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.addAllowedOriginPattern("*");
         configuration.addAllowedMethod("*");
         configuration.addAllowedHeader("*");
@@ -24,3 +28,4 @@ public class CorsConfig {
         return source;
     }
 }
+
